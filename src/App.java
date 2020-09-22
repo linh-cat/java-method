9626de788152772461887dafa01a0e2125cefe31
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2020-09-22 20:40:31 +0700

    add method return value

--- src/App.java
+++ src/App.java
@@ -1,19 +1,23 @@
 public class App {
-    static void showSum(double d, double e, int count) {
+    static void showSum(double d, double e, int count) { // no return value
         float sum = (float) (d + e);
         if (count < 1) {
             System.out.println("count smaller 1!");
         } else {
             for (int i = 0; i < count; i++) {
                 System.out.println(sum);
-
             }
             return;
         }
+    }
 
+    static double calculateInterest(double amt, double rate, int years) {
+        return amt * rate * years;
     }
 
     public static void main(String[] args) throws Exception {
-        showSum(7.5, 1.5, 2);
+        // showSum(7.5, 1.5, 2);
+        double result = calculateInterest(1.5, 2.5, 3);
+        System.out.println(result);
     }
 }
