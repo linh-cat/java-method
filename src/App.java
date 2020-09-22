c78d7349fb17173ea1344ade2ac6e74f3724a852
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2020-09-22 21:00:38 +0700

    add method return arr

--- src/App.java
+++ src/App.java
@@ -15,9 +15,23 @@ public class App {
         return amt * rate * years;
     }
 
+    static double[] produceInterestHistory(double amt, double rate, int years) {
+        double[] arr = new double[years];
+        for (int yearIndex = 0; yearIndex < years; yearIndex++) {
+            int year = yearIndex + 1;
+            arr[yearIndex] = calculateInterest(amt, rate, year);
+        }
+        return arr;
+    }
+
     public static void main(String[] args) throws Exception {
         // showSum(7.5, 1.5, 2);
-        double result = calculateInterest(1.5, 2.5, 3);
-        System.out.println(result);
+        // double result = calculateInterest(1.5, 2.5, 3);
+        // System.out.println(result);
+        double[] a;
+        a = produceInterestHistory(2.5, 1.2, 3);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + ",");
+        }
     }
 }
